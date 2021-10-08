8961fc19f85bb257f0f2a0bfca2f464f8ca33e28
Aboubacar TRAORE <stracsys@gmail.com>
2021-10-08 22:50:32 +0000

    modifié :         archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -41,7 +41,7 @@ then
 fi
 
 echo -e '\n\t Mirrorlist setup'
-pacman -Sy pacman-contrib
+pacman -S pacman-contrib
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 
